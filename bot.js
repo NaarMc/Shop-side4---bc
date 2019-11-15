e0909d08a75f2e9be37d6602a116f3f71ff4cc70
NaarMc <57398234+NaarMc@users.noreply.github.com>
2019-11-15 20:51:03 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -138,6 +138,20 @@ client.on('message' , message => {//bcrole
       message.channel.send(`**لقد تم ارسال هذه الرسالة الى ${message.guild.members.filter(m => m.roles.get(role.id)).size} عظو**`);
     }
 });
+client.on("message", message => {
+
+            if (message.content.startsWith(prefix + "bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};     
+});
+
 
 
 client.login(process.env.BOT_TOKEN);
